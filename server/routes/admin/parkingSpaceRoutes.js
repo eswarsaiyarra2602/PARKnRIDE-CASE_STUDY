5d8b62925493085454e1b0d120b6f7b23399f708
Eswar sai Yarra <eswarsaiyarra@Eswars-MacBook-Air.local>
2025-02-16 21:29:14 +0530

    minor fix : default export in parkingSpaceRoutes.js

--- server/routes/admin/parkingSpaceRoutes.js
+++ server/routes/admin/parkingSpaceRoutes.js
@@ -5,14 +5,10 @@ const {
   addParkingSpace, 
   updateParkingSpace, 
   deleteParkingSpace, 
-  getParkingSpaces
 } = require('../../controllers/parkingSpaceController');
-const { get } = require('mongoose');
-
 // Admin routes to add, update, and delete parking spaces for a particular location
 router.post('/', adminAuthMiddleware, addParkingSpace);
 router.put('/:locationCode', adminAuthMiddleware, updateParkingSpace);
 router.delete('/:locationCode', adminAuthMiddleware, deleteParkingSpace);
-router.get('/:locationCode', getParkingSpaces);
 
 module.exports = router;
